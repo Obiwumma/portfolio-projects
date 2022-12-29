d159da96d620bce4b74928522853e4ab62cff9d8
Obiwumma <111187407+Obiwumma@users.noreply.github.com>
2022-12-29 21:50:19 +0100

    Delete navbar.js

--- Q & A/navbar.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.querySelector(".navbar_toggle");
-const links = document.querySelector(".main_nav");
-
-navToggle.addEventListener('click', function(){
-    links.classList.toggle("show_nav");
-})
