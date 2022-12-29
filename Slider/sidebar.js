4c1bd4a54118c059877c4670984dd47440935c9b
Obiwumma <111187407+Obiwumma@users.noreply.github.com>
2022-12-29 22:04:32 +0100

    Add files via upload

---
+++ Slider/sidebar.js
@@ -0,0 +1,9 @@
+const openSidebar = () => {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+};
+
+const closeSidebar = () => {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+};
